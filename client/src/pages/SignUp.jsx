c09825d4c626cef3ea15b344b41a6ff36c2975b7
afeefnk <connectafeefnk@gmail.com>
2026-01-07 16:34:17 +0400

    complete sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -23,14 +23,14 @@ const SignUp = () => {
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 disabled:cursor-not-allowed">
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase duration-150 hover:opacity-95 disabled:opacity-80 disabled:cursor-not-allowed">
           Sign Up
         </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to="/sign-in">
-        <span className="text-blue-500">Sign In</span>
+          <span className="text-blue-500">Sign In</span>
         </Link>
       </div>
     </div>
