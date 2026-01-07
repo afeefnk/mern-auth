32e638e9a054206d795f678882ac8005e6ccc24d
afeefnk <connectafeefnk@gmail.com>
2026-01-07 22:30:14 +0400

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,3 +15,32 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    } 
+    const isPasswordValid = bcryptjs.compareSync(password, validUser.password);
+    if (!isPasswordValid) {
+      return next(errorHandler(401, "Invalid Credentials"));
+    }
+
+    const token = jwt.sign(
+      { id: validUser._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    const {password, ...otherDetails} = validUser._doc;
+
+    res.cookie("access_token", token, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 1000, // 1 hour
+    }).status(200).json(otherDetails);
+  } catch (error) {
+    next(error);
+  }
+}
